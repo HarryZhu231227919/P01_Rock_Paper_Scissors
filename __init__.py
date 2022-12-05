1454c0e3b605c4d1d6a306ab7e1220f46e82fbdb
wanyingl09 <wli30@stuy.edu>
2022-12-05 17:39:51 -0500

    trying out apis

--- __init__.py
+++ __init__.py
@@ -2,6 +2,9 @@ import requests
 
 url =  "https://api.edamam.com/api/recipes/v2"
 
-res = requests.get(url, params={'type':'public', 'app_id':"97c8bd4c", 'app_key':"99402c4f99681503751e94b03de8db33", 'cuisineType': "caribbean", 'q': "chicken and apple"})
+res = requests.get(url, params={'type':'public', 'app_id':"97c8bd4c", 'app_key':"99402c4f99681503751e94b03de8db33", 'q': "chicken and beef"})
 
-print(res.json()['hits'][0]['recipe']['label']) # prints the name of the first chicken recipe 
+#print(res.json()['hits'][0]['recipe']['label']) # prints the name of the first chicken recipe 
+print(res.json()['hits'][0]['recipe']['uri']) 
+print(res.json()['hits'][0]['recipe']['label']) 
+print(res.json()['hits'][0]['recipe']['cuisineType']) 
