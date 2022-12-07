f8ab9c1dd74c24605e1d8b0893c24eb2477caf8d
Selena Ho <sho30@stuy.edu>
2022-12-07 17:08:53 -0500

    update

--- app/__init__.py
+++ app/__init__.py
@@ -17,16 +17,36 @@ app = Flask(__name__)
 
 @app.route("/home", methods=['GET', 'POST'])
 def homePage():
-    return render_template("/templates/home.html", img_src="", recipe_title="")
+    return render_template("home.html", img_src="", recipe_title="")
 
 @app.route("/", methods=['GET', 'POST'])
 def login_page():
+    if (request.method == 'GET'): #just displayingthe login page
+        return render_template("login.html")
+    else:
+        return render_template("home.html") #placeholder until we get database methods
+        #check if user exists in database
+            #if user exists, check that password matches password in database
+                #if password does not match render_template for login.html with error message
+                #if password matches redirect to home page
+            #if user doesn't exist, render_template for login.html with error message
+
+@app.route("/register", methods=['GET', 'POST'])
+def register():
+    if (request.method == 'GET'):
+        return render_template("register.html")
+    #else:
+        #do the registering checks
+
+@app.route("/randRecipe", methods=['GET', 'POST'])
+def randRecipe():
+    if (request.method == 'GET'): #just displaying the random recipe page
+        return render_template("randrecipe.html") #need to send in the recipe title, image, and ingredients
+    #else: #user requested to see another random recipe
 
 @app.route("/specificRecipe", methods=['GET', 'POST'])
 def specificRecipe():
-
-
-
+    return render_template("specificrecipe.html")
 
 if __name__ == "__main__":
     app.debug = True
