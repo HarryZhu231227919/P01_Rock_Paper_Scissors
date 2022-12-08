b453c65e85996793e7d9cd1e9b6251800f054a0f
Selena Ho <sho30@stuy.edu>
2022-12-08 18:15:02 -0500

    login functioning

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,17 @@
 from flask import Flask, render_template, request
 from flask import session, redirect, url_for #not sure why this is not working
 import requests
+from app_db import *
 
 app = Flask(__name__)
+app.secret_key = "fsa932nds02ks3ld93nfjs02ns29rj"
+
 f=open('keys/key_edamam.txt', 'r') #accesses the file
 e_key=f.read() #edamam key
 f=open('keys/key_spoonacular.txt', 'r')
 s_key = f.read() #spoonacular key
 f=open('keys/id_edamam.txt')
 e_id = f.read()
-
 
 @app.route("/home", methods=['GET', 'POST'])
 def homePage():
@@ -23,15 +25,17 @@ def homePage():
 
 @app.route("/", methods=['GET', 'POST'])
 def login_page():
-    if (request.method == 'GET'): #just displayingthe login page
+    if( ("username" in session) and (user_exists(session.get("username"))) ): # checks to see if the user was already logged in
+        return redirect(url_for("homePage"))
+    if (request.method == 'GET'): #just displaying the login page
         return render_template("login.html")
-    else:
-        return render_template("home.html") #placeholder until we get database methods
-        #check if user exists in database
-            #if user exists, check that password matches password in database
-                #if password does not match render_template for login.html with error message
-                #if password matches redirect to home page
-            #if user doesn't exist, render_template for login.html with error message
+    else: #POST request made - user attempted to log in
+        if(user_exists(request.form.get("username"))):
+            if(checkuser(request.form.get("username"),request.form.get("password"))): #username matches the password
+                session["username"] = request.form.get("username")
+                return redirect(url_for("homePage"))
+            return render_template("login.html", error="Username and password don't match.")
+        return render_template("login.html", error="User does not exist.")
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
@@ -60,7 +64,12 @@ def specificRecipe():
         return render_template("specificrecipe.html")
     else:
         url = "https://api.edamam.com/api/recipes/v2"
-        requests.get(url, params={'type':'public', 'app_id':e_id, 'app_key':e_key, 'q': request.form[], }) 
+        #requests.get(url, params={'type':'public', 'app_id':e_id, 'app_key':e_key, 'q': request.form[], }) 
+
+@app.route("/logout")
+def logout():
+    session.pop("username",None) # removes session info
+    return redirect(url_for("login_page")) 
 
 if __name__ == "__main__":
     app.debug = True
--- app/app_db.py
+++ app/app_db.py
@@ -1,35 +1,49 @@
 import sqlite3
 
 DB_FILE = "SITE.db"
-db = sqlite3.connect(DB_FILE)
+db = sqlite3.connect(DB_FILE, check_same_thread=False)
 c = db.cursor()
 
 c.execute("CREATE TABLE if not Exists users(username TEXT, password TEXT);")
-
-
-db.commit()
-db.close()
+c.close()
 
 def checkuser(username, password): #checks if user's login is correct
-    db = sqlite3.connect(DB_FILE)
     c = db.cursor()
-    c.execute('SELECT password FROM users WHERE username = ?;', [username])
+    c.execute('SELECT password FROM users WHERE username = ?;', [str(username)])
     correct_password = c.fetchone() #fetches correct password from tuple
     if correct_password is None:
+        c.close()
         return False
     else:
+        c.close()
         return password == correct_password[0]
 
+def user_exists(username): #checks if a user exists
+    c = db.cursor()
+    result = c.execute("select username from users where username = ?", (str(username),))
+    try:
+        c.fetchone()[0] == username
+        c.close()
+        return True
+    except:
+        c.close()
+        return False
         
 #returns TRUE if another account exists with user, otherwise creates acc
 def create_acc(username, password): 
-    db = sqlite3.connect(DB_FILE)
     c = db.cursor()
-    c.execute('SELECT username FROM users WHERE username = ?;', [username])
-    check_user = c.fetchone();
+    c.execute('SELECT username FROM users WHERE username = ?;', [str(username)])
+    check_user = c.fetchone()
     if check_user is None:
-        c.execute('INSERT INTO users VALUES (?, ?);', (username, password) )
+        c.execute('INSERT INTO users VALUES (?, ?);', (str(username), str(password)) )
+        c.close()
         db.commit()
         return False
     else:
+        c.close()
         return True
+
+#print(create_acc("user", "pass"))
+#print(checkuser("user", "pas"))
+
+db.commit()
