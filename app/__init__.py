4b63481e5c5627717c4c385682f08e941ce85eb9
Selena Ho <sho30@stuy.edu>
2022-12-07 19:58:55 -0500

    testing

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,3 @@
-# import requests
-
-# url =  "https://api.edamam.com/api/recipes/v2"
-
-# res = requests.get(url, params={'type':'public', 'app_id':"97c8bd4c", 'app_key':"99402c4f99681503751e94b03de8db33", 'q': "biscuits and gravy", 'heatlh': "gluten-free"})
-
-# #print(res.json()['hits'][0]['recipe']['label']) # prints the name of the first chicken recipe 
-# print(res.json()['hits'][0]['recipe']['url']) 
-# print(res.json()['hits'][0]['recipe']['healthLabels']) 
-# print(res.json()['hits'][0]['recipe']['label']) 
-# print(res.json()['hits'][0]['recipe']['cuisineType']) 
-
 from flask import Flask, render_template, request
 from flask import session, redirect, url_for #not sure why this is not working
 import requests
---
+++ app/testing.py
@@ -0,0 +1,19 @@
+#testing the edamam API here
+# import requests
+
+# url =  "https://api.edamam.com/api/recipes/v2"
+
+# res = requests.get(url, params={'type':'public', 'app_id':"97c8bd4c", 'app_key':"99402c4f99681503751e94b03de8db33", 'q': ""})
+
+# #print(res.json()['hits'][0]['recipe']['label']) # prints the name of the first chicken recipe 
+# print(res.json())
+# print(res.json()['hits'][0]['recipe']['url']) 
+# print(res.json()['hits'][0]['recipe']['healthLabels']) 
+# print(res.json()['hits'][0]['recipe']['label']) 
+# print(res.json()['hits'][0]['recipe']['cuisineType']) 
+
+#testing the spoonacular API here: 
+# import requests
+# res = requests.get("https://api.spoonacular.com/recipes/random?apiKey=12643cbc7c944fc18030685f17426c85").json() #request to get random recipe
+# print(res.get('recipes')[0].get('title')) #gets the recipe title of that random recipe
+# print(res.get('recipes')[0].get('image')) #gets the recipe image of that random recipe
